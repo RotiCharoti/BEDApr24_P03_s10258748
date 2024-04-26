ee2c98052ad3549c91934e978fceb0cf70f7e7d2
Charlotte Lim <s10258748@connect.np.edu.sg>
2024-04-26 11:27:40 +0800

    colorOutput

--- week01/practical1/writingFile/app.js
+++ week01/practical1/writingFile/app.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 
 const content = 'Some content!';
 
-fs.writeFile('/Users/joe/test.txt', content, err => {
+fs.writeFile('test.txt', content, err => {
   if (err) {
     console.error(err);
   } else {
